a2d705be64136b4d2a59279eef1c8022e32f9f30
Andrey <3451z@mail.ru>
2022-02-09 12:49:30 +0300

    Initial Radio with lombok

---
+++ src/main/java/ru/netology/lombok/Radio.java
@@ -0,0 +1,75 @@
+package ru.netology.lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int minStation;
+    private int quantityStations = 10;
+    private int maxStation = calcMaxStation();
+    private int minVolume;
+    private int maxVolume = 100;
+
+    public int calcMaxStation() {
+        if (quantityStations > 0) {
+            return quantityStations - 1;
+        } else
+            return 0;
+    }
+
+    //for Station
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < minStation) {
+            return;
+        }
+        if (currentStation > maxStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation < maxStation) {
+            currentStation++;
+        } else {
+            currentStation = minStation;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation > minStation) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = maxStation;
+        }
+    }
+
+    //for Volume
+    public void currentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume & currentVolume >= minVolume) {
+            currentVolume++;
+        } else {
+            currentVolume = maxVolume;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume <= maxVolume & currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        } else {
+            currentVolume = minVolume;
+        }
+    }
+
+}
---
+++ src/test/java/ru/netology/lombok/RadioTest.java
@@ -0,0 +1,13 @@
+package ru.netology.lombok;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    @Test
+    public void shouldCreate(){
+        new Radio();
+    }
+
+}
